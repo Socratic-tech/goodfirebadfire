01ad78fb135810e37e247e596da43f2e729344b0
Socratic-tech <john.phillips@berrienresa.org>
2025-10-24 11:43:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // IMPORTANT for GitHub Project Pages:
+  base: '/goodfirebadfire/',   // <-- must match your repo name
 })
